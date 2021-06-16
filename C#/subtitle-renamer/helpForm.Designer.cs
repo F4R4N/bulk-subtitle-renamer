78ade032cc04a7eb76dd8c2f166cf582e10905bf
faran <farantgh@gmail.com>
2021-06-16 16:17:54 +0430

    build and other files

--- C#/subtitle-renamer/helpForm.Designer.cs
+++ C#/subtitle-renamer/helpForm.Designer.cs
@@ -29,6 +29,8 @@ namespace subtitle_renamer
 		private System.Windows.Forms.Label label13;
 		private System.Windows.Forms.Label label14;
 		private System.Windows.Forms.Label label15;
+		private System.Windows.Forms.Label label16;
+		private System.Windows.Forms.Label label17;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -67,13 +69,15 @@ namespace subtitle_renamer
 			this.label13 = new System.Windows.Forms.Label();
 			this.label14 = new System.Windows.Forms.Label();
 			this.label15 = new System.Windows.Forms.Label();
+			this.label16 = new System.Windows.Forms.Label();
+			this.label17 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(14, 9);
+			this.label1.Location = new System.Drawing.Point(14, 43);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(152, 17);
 			this.label1.TabIndex = 0;
@@ -83,7 +87,7 @@ namespace subtitle_renamer
 			// 
 			this.label2.AutoSize = true;
 			this.label2.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(172, 9);
+			this.label2.Location = new System.Drawing.Point(172, 43);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(430, 17);
 			this.label2.TabIndex = 1;
@@ -93,7 +97,7 @@ namespace subtitle_renamer
 			// 
 			this.label3.AutoSize = true;
 			this.label3.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.Location = new System.Drawing.Point(14, 36);
+			this.label3.Location = new System.Drawing.Point(14, 70);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(108, 17);
 			this.label3.TabIndex = 2;
@@ -103,7 +107,7 @@ namespace subtitle_renamer
 			// 
 			this.label4.AutoSize = true;
 			this.label4.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label4.Location = new System.Drawing.Point(128, 36);
+			this.label4.Location = new System.Drawing.Point(128, 70);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(275, 17);
 			this.label4.TabIndex = 3;
@@ -113,7 +117,7 @@ namespace subtitle_renamer
 			// 
 			this.label5.AutoSize = true;
 			this.label5.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label5.Location = new System.Drawing.Point(14, 64);
+			this.label5.Location = new System.Drawing.Point(14, 98);
 			this.label5.Name = "label5";
 			this.label5.Size = new System.Drawing.Size(165, 17);
 			this.label5.TabIndex = 4;
@@ -123,7 +127,7 @@ namespace subtitle_renamer
 			// 
 			this.label6.AutoSize = true;
 			this.label6.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label6.Location = new System.Drawing.Point(185, 64);
+			this.label6.Location = new System.Drawing.Point(185, 98);
 			this.label6.Name = "label6";
 			this.label6.Size = new System.Drawing.Size(414, 17);
 			this.label6.TabIndex = 5;
@@ -133,7 +137,7 @@ namespace subtitle_renamer
 			// 
 			this.label7.AutoSize = true;
 			this.label7.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label7.Location = new System.Drawing.Point(14, 91);
+			this.label7.Location = new System.Drawing.Point(14, 125);
 			this.label7.Name = "label7";
 			this.label7.Size = new System.Drawing.Size(101, 17);
 			this.label7.TabIndex = 6;
@@ -143,7 +147,7 @@ namespace subtitle_renamer
 			// 
 			this.label8.AutoSize = true;
 			this.label8.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label8.Location = new System.Drawing.Point(121, 91);
+			this.label8.Location = new System.Drawing.Point(121, 125);
 			this.label8.Name = "label8";
 			this.label8.Size = new System.Drawing.Size(565, 17);
 			this.label8.TabIndex = 7;
@@ -154,7 +158,7 @@ namespace subtitle_renamer
 			// 
 			this.label9.AutoSize = true;
 			this.label9.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label9.Location = new System.Drawing.Point(14, 119);
+			this.label9.Location = new System.Drawing.Point(14, 153);
 			this.label9.Name = "label9";
 			this.label9.Size = new System.Drawing.Size(165, 17);
 			this.label9.TabIndex = 8;
@@ -164,7 +168,7 @@ namespace subtitle_renamer
 			// 
 			this.label10.AutoSize = true;
 			this.label10.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label10.Location = new System.Drawing.Point(185, 119);
+			this.label10.Location = new System.Drawing.Point(185, 153);
 			this.label10.Name = "label10";
 			this.label10.Size = new System.Drawing.Size(440, 34);
 			this.label10.TabIndex = 9;
@@ -175,7 +179,7 @@ namespace subtitle_renamer
 			// 
 			this.label11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
 			this.label11.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label11.Location = new System.Drawing.Point(0, 238);
+			this.label11.Location = new System.Drawing.Point(0, 249);
 			this.label11.Name = "label11";
 			this.label11.Size = new System.Drawing.Size(799, 2);
 			this.label11.TabIndex = 10;
@@ -185,17 +189,17 @@ namespace subtitle_renamer
 			// 
 			this.label12.AutoSize = true;
 			this.label12.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label12.Location = new System.Drawing.Point(14, 249);
+			this.label12.Location = new System.Drawing.Point(14, 268);
 			this.label12.Name = "label12";
-			this.label12.Size = new System.Drawing.Size(365, 17);
+			this.label12.Size = new System.Drawing.Size(383, 17);
 			this.label12.TabIndex = 11;
-			this.label12.Text = "episode index in video name or subtitle name:";
+			this.label12.Text = "episode number in video name or subtitle name:";
 			// 
 			// label13
 			// 
 			this.label13.AutoSize = true;
 			this.label13.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label13.Location = new System.Drawing.Point(14, 276);
+			this.label13.Location = new System.Drawing.Point(14, 295);
 			this.label13.Name = "label13";
 			this.label13.Size = new System.Drawing.Size(438, 170);
 			this.label13.TabIndex = 12;
@@ -205,7 +209,7 @@ namespace subtitle_renamer
 			// 
 			this.label14.AutoSize = true;
 			this.label14.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label14.Location = new System.Drawing.Point(14, 172);
+			this.label14.Location = new System.Drawing.Point(14, 206);
 			this.label14.Name = "label14";
 			this.label14.Size = new System.Drawing.Size(180, 17);
 			this.label14.TabIndex = 13;
@@ -215,18 +219,40 @@ namespace subtitle_renamer
 			// 
 			this.label15.AutoSize = true;
 			this.label15.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label15.Location = new System.Drawing.Point(200, 172);
+			this.label15.Location = new System.Drawing.Point(200, 206);
 			this.label15.Name = "label15";
 			this.label15.Size = new System.Drawing.Size(455, 34);
 			this.label15.TabIndex = 14;
 			this.label15.Text = "subtitle file extension. there are several file formats to choose. \r\ne.g.(srt, as" +
 	"s, etc.)";
 			// 
+			// label16
+			// 
+			this.label16.AutoSize = true;
+			this.label16.Font = new System.Drawing.Font("Verdana", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label16.Location = new System.Drawing.Point(13, 14);
+			this.label16.Name = "label16";
+			this.label16.Size = new System.Drawing.Size(50, 17);
+			this.label16.TabIndex = 15;
+			this.label16.Text = "Note:\r\n";
+			// 
+			// label17
+			// 
+			this.label17.AutoSize = true;
+			this.label17.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label17.Location = new System.Drawing.Point(68, 13);
+			this.label17.Name = "label17";
+			this.label17.Size = new System.Drawing.Size(334, 17);
+			this.label17.TabIndex = 16;
+			this.label17.Text = "put only one season of series in the directory.";
+			// 
 			// HelpForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(699, 470);
+			this.ClientSize = new System.Drawing.Size(699, 481);
+			this.Controls.Add(this.label17);
+			this.Controls.Add(this.label16);
 			this.Controls.Add(this.label15);
 			this.Controls.Add(this.label14);
 			this.Controls.Add(this.label13);
